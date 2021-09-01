a4f20bb55ea3d2ab729c832d614734475f1dc3b6
alouette <tbandiougou3@gmail.com>
2021-09-01 14:20:28 +0200

    contact fait

---
+++ contact.php
@@ -0,0 +1,8 @@
+<h1>Nous contact</h1>
+<p>
+    24-26 bd Chastenet de Géry
+    94814 Villejuif Cedex
+    Tél : 01 46 78 01 00
+    Fax : 01 46 78 50 35
+    paulguinot@guinot.asso.fr
+</p>
--- index.php
+++ index.php
@@ -4,41 +4,50 @@
 
 <head>
   <title>Gestion des inscription</title>
-  <meta charset="utf-8"/>
-   <link rel="stylesheet" type="text/css" href="styleIndex.css">
+  <meta charset="utf-8" />
+  <link rel="stylesheet" type="text/css" href="styleIndex.css">
 </head>
 
 <body>
-    <div id="bloc_page" >
-        <header>
-           <div class="logo"><img src="images/gecos.png" width="200" height="200" alt="Logo de Gecos"></div>
-           <h1>Une école d'élite </h1>
-        </header>
+  <div id="bloc_page">
+    <header>
+      <div class="logo"><img src="images/gecos.png" width="200" height="200" alt="Logo de Gecos"></div>
+      <h1>Une école d'élite </h1>
+    </header>
 
-        <hr style="width: 100%; height: 10px">
-         <center>
-             <section>
-                   <div class="inscrire"><a href="inscrire.php" ><img src="images/inscription.jpg" width="256" height="200" alt="Logo Inscription"></a></div>
-                   <br><br><br>
+    <hr style="width: 100%; height: 10px">
+    <center>
+      <section>
+        <div class="inscrire"><a href="inscrire.php"><img src="images/inscription.jpg" width="256" height="200" alt="Logo Inscription"></a></div>
+        <br><br><br>
 
 
-                 <div class="modifier"><div><img src="images/modifier.png" width="220" height="200" alt="Logo Modifier"></div>
-                 <h3><a href="modifier.php">Modifier</a></h3> </div>
+        <div class="modifier">
+          <div><img src="images/modifier.png" width="220" height="200" alt="Logo Modifier"></div>
+          <h3><a href="modifier.php">Modifier</a></h3>
+        </div>
 
-                 <div class="supprimer"><div><a href="supprimer.php"><img src="images/supprimer.png" width="256" height="256" alt="Logo Supprimer"></a></div>
-                 <h3><a href="supprimer.php">SUPPRIMER</h3> </div>
+        <div class="supprimer">
+          <div><a href="supprimer.php"><img src="images/supprimer.png" width="256" height="256" alt="Logo Supprimer"></a></div>
+          <h3><a href="supprimer.php">SUPPRIMER</h3>
+        </div>
 
-                 <div class="afficher"><div><a href="liste.php"><img src="images/afficher.png" width="220" height="220" alt="Logo Afficher"></a></div>
-                 <h3><a href="liste.php">AFFICHER</h3> </div>
-               </div>
+        <div class="afficher">
+          <div><a href="liste.php"><img src="images/afficher.png" width="220" height="220" alt="Logo Afficher"></a></div>
+          <h3><a href="liste.php">AFFICHER</h3>
+        </div>
+  
 
-             </section>
-            </center>
-              <hr style="width: 900px; height: 10px" >
-                <footer>
-                    <h2>CHOISIR GECOS FORMATION C'EST CONTRUIT SON AVENIRE</h2>
-                </footer>
-    </div>
+  <div>
+    <h3><a href="contact.php">Nous Contact</h3>
+  </div>
+  </section>
+  </center>
+  <hr style="width: 900px; height: 10px">
+  <footer>
+    <h2>CHOISIR GECOS FORMATION C'EST CONTRUIT SON AVENIRE</h2>
+  </footer>
+  </div>
 </body>
 
 </html>
